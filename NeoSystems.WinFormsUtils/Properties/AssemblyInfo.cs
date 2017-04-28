f52d1411e405553869d2fb9b1f4ff8b7155d2010
Armand Jordaan <armand@neosystems.co.za>
2017-04-28 03:40:09 +0200

    First version

--- NeoSystems.WinFormsUtils/Properties/AssemblyInfo.cs
+++ NeoSystems.WinFormsUtils/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NeoSystems.WinFormsUtils")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Windows Forms Utilities")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Neo Systems")]
 [assembly: AssemblyProduct("NeoSystems.WinFormsUtils")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyInformationalVersion("0.0.1-alpha")]
